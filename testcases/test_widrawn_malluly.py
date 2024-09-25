e2f67fbf35171b34f410e12f52e559ed43197774
tej.pratap <golusingh19479@gmail.com>
2024-09-25 14:38:50 +0530

    all withd

--- testcases/test_widrawn_malluly.py
+++ testcases/test_widrawn_malluly.py
@@ -16,16 +16,117 @@ class TestManualWithdrawn:
         home_page.log_button()
         home_page.enter_username("tej.pratap.admin@faircent.com")
         home_page.enter_password("tej.pratap.admin@faircent.com")
-        #home_page.enter_username("Fc_admin")
-        #home_page.enter_password("techlet@123")
-        #query for investment dta check
-        #select uid,unique_user_id as investment_id,(wallet-locked_amount) as amount from cent_virtual_account where unique_user_id in (1000213431,1000138302) and (wallet-locked_amount)>0
+        # home_page.enter_username("Fc_admin")
+        # home_page.enter_password("techlet@123")
 
         home_page.click_login()
         # Open the manual withdrawal page in a new tab
         withdrawal_data = [
-            {"investment_id": "1000257828", "amount": "305650.68", "uid": 4864135},
-
+            {
+                "investment_id": 1000015017,
+                "uid": 120820,
+                "amount": 56347.54
+            },
+            {
+                "investment_id": 1000068199,
+                "uid": 560889,
+                "amount": 384602.21
+            },
+            {
+                "investment_id": 1000104264,
+                "uid": 828421,
+                "amount": 1932.77
+            },
+            {
+                "investment_id": 1000117710,
+                "uid": 1073933,
+                "amount": 62269.13
+            },
+            {
+                "investment_id": 1000165881,
+                "uid": 3535961,
+                "amount": 110959.43
+            },
+            {
+                "investment_id": 1000170071,
+                "uid": 3674105,
+                "amount": 110959.43
+            },
+            {
+                "investment_id": 1000176216,
+                "uid": 3842496,
+                "amount": 401754.14
+            },
+            {
+                "investment_id": 1000176462,
+                "uid": 3847934,
+                "amount": 105863.01
+            },
+            {
+                "investment_id": 1000179436,
+                "uid": 3919305,
+                "amount": 118783.2
+            },
+            {
+                "investment_id": 1000181940,
+                "uid": 4000686,
+                "amount": 125269.42
+            },
+            {
+                "investment_id": 1000183354,
+                "uid": 4054814,
+                "amount": 137876.71
+            },
+            {
+                "investment_id": 1000184556,
+                "uid": 4090845,
+                "amount": 137876.71
+            },
+            {
+                "investment_id": 1000186483,
+                "uid": 4168567,
+                "amount": 464942.86
+            },
+            {
+                "investment_id": 1000199861,
+                "uid": 4418740,
+                "amount": 81534.25
+            },
+            {
+                "investment_id": 1000201963,
+                "uid": 4432881,
+                "amount": 44728.76
+            },
+            {
+                "investment_id": 1000202033,
+                "uid": 4433635,
+                "amount": 28032.88
+            },
+            {
+                "investment_id": 1000207251,
+                "uid": 4471550,
+                "amount": 10136.99
+            },
+            {
+                "investment_id": 1000231606,
+                "uid": 4635564,
+                "amount": 108493.15
+            },
+            {
+                "investment_id": 1000252950,
+                "uid": 4799647,
+                "amount": 89268.49
+            },
+            {
+                "investment_id": 1000254761,
+                "uid": 4817960,
+                "amount": 87452.06
+            },
+            {
+                "investment_id": 1000259311,
+                "uid": 4875207,
+                "amount": 78345.21
+            }
         ]
 
         # Open the manual withdrawal page in a new tab and perform the tasks
@@ -74,12 +175,11 @@ class TestManualWithdrawn:
         withdrawn_submit = self.driver.find_element(By.ID, "edit-submit")
         use = False
         if amount_value and float(amount_value) > 0:
-            use=True
+            use = True
         if use and not checkbox.is_selected():
             if checkbox.is_selected():
                 checkbox.click()
             enter_amount.send_keys(amount_value)
-            time.sleep(10)
 
             withdrawn_submit.click()
         else:
